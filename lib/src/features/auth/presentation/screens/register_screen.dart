6d46d360b6e73b2fb0a875eed75dd1ae28e9febe
dieudonneiot <arduinoelectronic108@gmail.com>
2026-01-30 15:26:38 +0300

    commit_15h25_30jan26

--- lib/src/features/auth/presentation/screens/register_screen.dart
+++ lib/src/features/auth/presentation/screens/register_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../../../core/routing/routes.dart';
 import '../../../../core/supabase/supabase_service.dart';
